5772824d74153fb936a8840911716feb71924a50
pram <pram@attale.com>
2015-06-02 11:20:20 +0100

    choosy conditional check

---
+++ src/main/java/com/naughtyzombie/ocp/Choosy.java
@@ -0,0 +1,20 @@
+package com.naughtyzombie.ocp;
+
+public class Choosy {
+    public static void main(String[] args) {
+        String result = "";
+        int x = 7, y = 8;
+        if (x == 3) {
+            result += "1";
+        } else if (x > 9) {
+            result += "2";
+        } else if (y < 9) {
+            result += "3";
+        } else if (x == 7) {
+            result += "4";
+        } else {
+            result += "5";
+        }
+        System.out.println(result);
+    }
+}
