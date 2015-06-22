4638a2bf021562d44b0b9e8c1de379ea293c275b
pram <pram@attale.com>
2015-06-22 13:42:24 +0100

    formatting

--- src/main/java/com/naughtyzombie/fib/FibonnaciLoop.java
+++ src/main/java/com/naughtyzombie/fib/FibonnaciLoop.java
@@ -7,6 +7,12 @@ public class FibonnaciLoop {
     public static void main(String[] args) {
         int n = 25, first = 0, second = 1, next, i;
         System.out.println("Printing first " + n + " numbers in Fibonacci Series \n");
+        calc(n, first, second);
+    }
+
+    private static void calc(int n, int first, int second) {
+        int i;
+        int next;
         for (i = 0; i < n; i++) {
             if (i <= 1)
                 next = i;
