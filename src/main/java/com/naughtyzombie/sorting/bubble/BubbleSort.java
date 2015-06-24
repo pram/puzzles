474358b923f26e0ba98fa49944091a310a782ab8
pram <pram@attale.com>
2015-06-24 13:40:24 +0100

    bubble sort implementation

---
+++ src/main/java/com/naughtyzombie/sorting/bubble/BubbleSort.java
@@ -0,0 +1,43 @@
+package com.naughtyzombie.sorting.bubble;
+
+/**
+ * Created by pram on 24/06/2015.
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] input = {4, 2, 9, 6, 23, 12, 34, 0, 1, 4, 6, 3};
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSort(input);
+    }
+
+    private void bubbleSort(int[] input) {
+        int n = input.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i=0; i < n -1; i++) {
+                k = i +1;
+                if (input[i] > input[k]) {
+                    swapNumbers(i,k,input);
+                }
+            }
+            printNumbers(input);
+        }
+    }
+
+    private void printNumbers(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int out = input[i];
+            System.out.print(out + ", ");
+        }
+        System.out.print("\n");
+    }
+
+    private void swapNumbers(int i, int k, int[] input) {
+        int temp;
+        temp = input[i];
+        input[i]  = input[k];
+        input[k] = temp;
+    }
+
+
+}
