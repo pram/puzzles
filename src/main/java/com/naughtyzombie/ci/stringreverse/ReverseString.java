0ec3767b8425c99594d6590a9b6e61f6d36018bc
pram <pram@attale.com>
2015-07-02 11:28:04 +0100

    reverse string

---
+++ src/main/java/com/naughtyzombie/ci/stringreverse/ReverseString.java
@@ -0,0 +1,33 @@
+package com.naughtyzombie.ci.stringreverse;
+
+/**
+ * Created by pram on 02/07/2015.
+ */
+public class ReverseString {
+    public static void main(String[] args) {
+        ReverseString rs = new ReverseString();
+        String input = "This is a test string to reverse";
+        String output = rs.reverseString1(input);
+        char[] output2 = rs.reverseString2(input.toCharArray());
+        System.out.println(output);
+        System.out.println(output2);
+    }
+
+    private char[] reverseString2(char[] input) {
+        for(int i = 0, j = input.length - 1; i < input.length / 2; i++, j--) {
+            char c = input[i];
+            input[i] = input[j];
+            input[j] = c;
+        }
+        return input;
+    }
+
+
+    private String reverseString1(String input) {
+        String reversed = "";
+        for(int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        return reversed;
+    }
+}
