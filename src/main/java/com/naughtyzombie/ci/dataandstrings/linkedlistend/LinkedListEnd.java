2bf9ea171427c8e832d2a0c1040d4e396a856d09
pram <pram@attale.com>
2015-07-03 12:28:14 +0100

    linked list

---
+++ src/main/java/com/naughtyzombie/ci/dataandstrings/linkedlistend/LinkedListEnd.java
@@ -0,0 +1,16 @@
+package com.naughtyzombie.ci.dataandstrings.linkedlistend;
+
+import com.naughtyzombie.ci.lib.AssortedMethods;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Created by pram on 03/07/2015.
+ */
+public class LinkedListEnd {
+    public static void main(String[] args) {
+        LinkedList<String> listOfWords = new LinkedList<>(Arrays.asList(AssortedMethods.getListOfWords()));
+
+    }
+}
