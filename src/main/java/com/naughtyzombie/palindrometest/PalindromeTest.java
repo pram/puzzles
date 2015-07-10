58abfc7cd31187ac7277c02fc69235818b801439
pram <pram@attale.com>
2015-07-10 16:39:48 +0100

    adding palindrome test

--- src/main/java/com/naughtyzombie/palindrometest/PalindromeTest.java
+++ src/main/java/com/naughtyzombie/palindrometest/PalindromeTest.java
@@ -4,4 +4,26 @@ package com.naughtyzombie.palindrometest;
  * Created by pram on 09/07/2015.
  */
 public class PalindromeTest {
+    public static void main(String[] args) {
+        PalindromeTest pt = new PalindromeTest();
+        boolean isPalindrome = pt.isPalindrome("aabbgagbbaa");
+        System.out.println(isPalindrome);
+    }
+
+    private boolean isPalindrome(String testString) {
+        if (testString.length() < 2)
+            return false;
+
+        char[] chars = testString.toCharArray();
+        boolean retVal = true;
+        for (int i = 0; i < chars.length/2; i++) {
+            char left = chars[i];
+            char right = chars[(chars.length - 1) - i];
+            if (left != right) {
+                retVal = false;
+                break;
+            }
+        }
+        return retVal;
+    }
 }
